bf7de367ea30d35c3f08fc538e0483ad37337a27
SenaMurakami <sena@emishi.co.jp>
2024-03-18 23:26:34 +0900

    Fetch all data from the Pokemon API and display it

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark">
+        <main className="flex min-h-screen flex-col items-center p-24">
+          <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+            <h2 className="text-2xl text-bold">PokemonFinder</h2>
+          </div>
           {children}
+          </main>
         </ThemeProvider>
       </body>
     </html>
---
+++ components/pokemon-grid.tsx
@@ -0,0 +1,43 @@
+"use client"
+import { PokemonCard } from "@/components/pokemon-card";
+import { useState } from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+
+// <PokemonGrid pokemonList={data} />
+
+interface PokemonGridProps {
+  pokemonList: any
+}
+
+export function PokemonGrid({ pokemonList } : PokemonGridProps) {
+  const [ searchText, setSearchText ] = useState("");
+
+  console.log(pokemonList)
+
+  return (
+    <>
+      <div>
+        <h3 className="text-2xl py-6 text-center">Search For Your Pokemon!</h3>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="pokemonName">Pokemon Name</Label>
+          <Input 
+            type="text" 
+            value={searchText} 
+            autoComplete="off"
+            id="pokemonName"
+            placeholder="Charizard, Pikachu, etc."
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </div>
+        <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon Collection</h3>
+      </div>
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
+        {pokemonList.map((pokemon : any) => {
+          return <PokemonCard name={pokemon.name} />
+        })}
+
+      </div>
+    </>
+  )
+}
